6e7e704ac1bc42baaead199aa730d1605fee0cc2
Wolfram Kriesing <wk@uxebu.com>
2014-08-05 00:37:51 +0200

    Handle multiple keys shortcut.

--- test/keyboard-shortcut-spec.js
+++ test/keyboard-shortcut-spec.js
@@ -15,10 +15,17 @@ define(function() {
   });
 
   describe('simple keyboard shortcut', function() {
-    it('should call according callback', function() {
+    it('with one key should call according callback', function() {
       var callback = jasmine.createSpy('callback');
       var shortcut = ['Meta', 'S'];
-      mapShortcut(shortcut, callback);
+      mapShortcuts([[shortcut, callback]]);
+      pressKeysAndKeyUp(shortcut);
+      expect(callback).toHaveBeenCalled();
+    });
+    it('with two keys should call according callback', function() {
+      var callback = jasmine.createSpy('callback');
+      var shortcut = ['Meta', 'I', 'I'];
+      mapShortcuts([[shortcut, callback]]);
       pressKeysAndKeyUp(shortcut);
       expect(callback).toHaveBeenCalled();
     });
@@ -26,20 +33,40 @@ define(function() {
   
 });
 
-function mapShortcut(shortcut, callback) {
-  var pressedKeys = [];
+function ShortcutManager() {
+  keyboardUtil.addKeyDownListener(this._keyDown.bind(this));
+  keyboardUtil.addKeyUpListener(this._keyUp.bind(this));
+}
+ShortcutManager.prototype = {
+  _pressedKeys: [],
+  _registeredShortcuts: [],
+  registerShortcut: function(shortcut, callback) {
+    this._registeredShortcuts.push([shortcut, callback]);
+  },
 
-  function keyDown(key) {
-    pressedKeys.push(key);
-  }
+  _keyDown: function(key) {
+    this._pressedKeys.push(key);
+  },
 
-  function keyUp() {
-    if (shortcut.join('+') == pressedKeys.join('+')) {
-      callback();
+  _keyUp: function() {
+    var pressedKeys = this._pressedKeys;
+    var found = this._registeredShortcuts.filter(function(shortcut) {
+      return shortcut[0].join('+') == pressedKeys.join('+');
+    });
+    if (found.length) {
+      // Use the first shortcut map found.
+      var firstShortcut = found[0];
+      firstShortcut[1]();
     }
+    this._pressedKeys = [];
   }
-  keyboardUtil.addKeyDownListener(keyDown);
-  keyboardUtil.addKeyUpListener(keyUp);
+};
+
+function mapShortcuts(shortcuts) {
+  var manager = new ShortcutManager();
+  shortcuts.forEach(function(shortcut) {
+    manager.registerShortcut(shortcut[0], shortcut[1]);
+  });
 }
 
 var keyboardUtil = {
