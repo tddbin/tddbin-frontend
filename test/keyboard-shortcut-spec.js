046b9404241331238ba219087ce4b8b1cce4612c
Wolfram Kriesing <wk@uxebu.com>
2014-08-04 23:11:48 +0200

    Start with keyboard shortcut implementation.

---
+++ test/keyboard-shortcut-spec.js
@@ -0,0 +1,17 @@
+/*
+ * - key combo should fire on keyup
+ * -
+ *
+ **/
+
+if (typeof define !== 'function') { var define = require('amdefine')(module) }
+
+define(function() {
+
+  describe('tests suite', function() {
+    it('should execute', function() {
+      expect(true).toBe(true);
+    });
+  });
+  
+});
