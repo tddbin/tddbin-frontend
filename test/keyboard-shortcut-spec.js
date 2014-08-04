183459994bbced790be8951fe3bbfa39411b5b8a
Wolfram Kriesing <wk@uxebu.com>
2014-08-04 23:59:04 +0200

    Implement Meta+S shortcut.

--- test/keyboard-shortcut-spec.js
+++ test/keyboard-shortcut-spec.js
@@ -1,6 +1,6 @@
 /*
- * - key combo should fire on keyup
- * -
+ * - key combo Meta+S should fire on keyup
+ * - key combo Meta+I+I should fire on keyup
  *
  **/
 
@@ -13,5 +13,53 @@ define(function() {
       expect(true).toBe(true);
     });
   });
+
+  describe('simple keyboard shortcut', function() {
+    it('should call according callback', function() {
+      var callback = jasmine.createSpy('callback');
+      var shortcut = ['Meta', 'S'];
+      mapShortcut(shortcut, callback);
+      pressKeysAndKeyUp(shortcut);
+      expect(callback).toHaveBeenCalled();
+    });
+  });
   
 });
+
+function mapShortcut(shortcut, callback) {
+  var pressedKeys = [];
+
+  function keyDown(key) {
+    pressedKeys.push(key);
+  }
+
+  function keyUp() {
+    if (shortcut.join('+') == pressedKeys.join('+')) {
+      callback();
+    }
+  }
+  keyboardUtil.addKeyDownListener(keyDown);
+  keyboardUtil.addKeyUpListener(keyUp);
+}
+
+var keyboardUtil = {
+  addKeyDownListener: function() {},
+  addKeyUpListener: function() {}
+};
+
+// test utils
+
+var keyDownListeners = [];
+var keyUpListeners = [];
+spyOn(keyboardUtil, 'addKeyDownListener').andCallFake(function(fn) {
+  keyDownListeners.push(fn);
+});
+spyOn(keyboardUtil, 'addKeyUpListener').andCallFake(function(fn) {
+  keyUpListeners.push(fn);
+});
+function pressKeysAndKeyUp(keys) {
+  keys.forEach(function(key) {
+    keyDownListeners[0](key);
+  });
+  keyUpListeners[0] && keyUpListeners[0]();
+}
