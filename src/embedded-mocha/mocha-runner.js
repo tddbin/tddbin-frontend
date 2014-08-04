07077cf6e6b057d6c27f46349907a34a91787053
Wolfram Kriesing <wk@uxebu.com>
2014-08-04 11:33:50 +0200

    Move all runner code in the file.

--- src/embedded-mocha/mocha-runner.js
+++ src/embedded-mocha/mocha-runner.js
@@ -6,14 +6,33 @@ define([
   Iframe
 ){
 
-  function MochaRunner(domNode) {
+  function MochaRunner(domNode, eventReceiver) {
     this._domNode = domNode;
+    (eventReceiver || window).addEventListener('message', this.handleDataReceived.bind(this), false);
   }
 
   MochaRunner.prototype = {
 
     render: function() {
-      React.renderComponent(Iframe(), this._domNode);
+      var iframe = React.renderComponent(Iframe(), this._domNode);
+      this._iframeRef = iframe.getIframeRef();
+    },
+
+    send: function(sourceCode) {
+      var iframe = this._iframeRef.contentWindow;
+      iframe.postMessage(sourceCode, '*');
+    },
+
+    _onStats: null,
+    onStats: function(fn) {
+      this._onStats = fn;
+    },
+
+    handleDataReceived: function(data) {
+      if (this._onStats) {
+        var stats = data.data;
+        this._onStats(stats);
+      }
     }
 
   };
