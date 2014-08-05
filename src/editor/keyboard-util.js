3c91fc18d60ecbb2462c3b153034fdef20297a37
Wolfram Kriesing <wk@uxebu.com>
2014-08-05 03:45:26 +0200

    Implement the event listeners for keyUp and keyDown.

--- src/editor/keyboard-util.js
+++ src/editor/keyboard-util.js
@@ -3,8 +3,18 @@ if (typeof define !== 'function') { var define = require('amdefine')(module) }
 define(function() {
 
   var keyboardUtil = {
-    addKeyDownListener: function() {},
-    addKeyUpListener: function() {}
+    addKeyDownListener: function(fn) {
+      document.addEventListener('keydown', function(evt) {
+        fn(evt.keyCode);
+//        evt.preventDefault();
+      });
+    },
+
+    addKeyUpListener: function(fn) {
+      document.addEventListener('keyup', function(evt) {
+        fn(evt.keyCode);
+      });
+    }
   };
 
   return keyboardUtil;
