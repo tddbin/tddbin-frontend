882d9abf6c72c7f2f8e10e86ba78889a8e020f3c
Wolfram Kriesing <wk@uxebu.com>
2014-08-07 13:39:50 +0200

    Remove unused code.

--- src/keyboard-shortcut/test/util.js
+++ src/keyboard-shortcut/test/util.js
@@ -1,25 +1,8 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module) }
 
 define([
-  '../shortcut-manager'
 ],function(
-  ShortcutManager
 ) {
-
-  var keyCodeMap = ShortcutManager._keyCodeToReadableKeyMap;
-
-  function fromKeyNameToKeyCode(keyName) {
-    for (var keyCode in keyCodeMap) {
-      if (keyName === keyCodeMap[keyCode]) {
-        return keyCode;
-      }
-    }
-    return (''+keyName).charCodeAt(0);
-  }
-
-  function toKeyCodes(keyNames) {
-    return keyNames.map(fromKeyNameToKeyCode);
-  }
 
   function KeyPressEmulation(keyboardEventUtil) {
     this._keyDownListeners = [];
@@ -65,7 +48,6 @@ define([
   };
 
   return {
-    toKeyCodes: toKeyCodes,
     KeyPressEmulation: KeyPressEmulation
   }
 });
