785078eaf27670b27208921dfca73f10249107c9
Wolfram Kriesing <wk@uxebu.com>
2014-08-26 00:31:13 +0200

    All requirejs is gone now :).

--- src/keyboard-shortcut-overlay/overlay.js
+++ src/keyboard-shortcut-overlay/overlay.js
@@ -1,32 +1,26 @@
-define([
-  'react',
-  'jsx!./component'
-], function(
-  React,
-  OverlayComponent
-){
+var React = require('react');
+var OverlayComponent = require('./component');
 
-  function Overlay(domNode) {
-    this._domNode = domNode;
+function Overlay(domNode) {
+  this._domNode = domNode;
+}
+
+Overlay.prototype = {
+
+  _component: null,
+
+  render: function(shortcuts) {
+    this._component = React.renderComponent(OverlayComponent({shortcuts: shortcuts}), this._domNode);
+  },
+
+  show: function() {
+    this._component.show();
+  },
+
+  hide: function() {
+    this._component.hide();
   }
 
-  Overlay.prototype = {
+};
 
-    _component: null,
-
-    render: function(shortcuts) {
-      this._component = React.renderComponent(OverlayComponent({shortcuts: shortcuts}), this._domNode);
-    },
-
-    show: function() {
-      this._component.show();
-    },
-
-    hide: function() {
-      this._component.hide();
-    }
-
-  };
-
-  return Overlay;
-});
+module.exports = Overlay;
