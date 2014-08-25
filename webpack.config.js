6915ee50f13cd60aa82c8e36122804941e46b369
Wolfram Kriesing <wk@uxebu.com>
2014-08-26 00:17:23 +0200

    Consistently name built files.

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ module.exports = {
   entry: {
     // Put all entry modules that are also required by another entry module into an array
     // see https://github.com/webpack/webpack/issues/300
-    'mocha-runner': ['./src/test-runner/mocha/runner.js'],
-    'test-runner-mocha-spec-runner': './src/test-runner/mocha/spec-runner.js',
+    'src-test-runner-mocha-runner': ['./src/test-runner/mocha/runner.js'],
+    'src-test-runner-mocha-spec-runner': './src/test-runner/mocha/spec-runner.js',
 
-    'examples-test-runner-mocha': './examples/test-runner/mocha.js'
+    'examples-test-runner-mocha': './examples/test-runner/mocha.js',
+    'examples-editor': './examples/editor.js'
   },
   output: {
     path: path.join(__dirname, 'dist'),
