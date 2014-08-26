1f369c2084c176d4d55bbe7eccba43f5b29c7784
Wolfram Kriesing <wk@uxebu.com>
2014-08-26 23:27:30 +0200

    Make it work in the webpack-dev-server, looks much better anyways :).

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ module.exports = {
   entry: {
     // Put all entry modules that are also required by another entry module into an array
     // see https://github.com/webpack/webpack/issues/300
-    'src-test-runner-mocha-runner': ['./src/test-runner/mocha/runner.js'],
-    'src-test-runner-mocha-spec-runner': './src/test-runner/mocha/spec-runner.js',
+//    'src-test-runner-mocha-runner': ['./src/test-runner/mocha/runner.js'],
+    'src/test-runner/mocha/spec-runner': './src/test-runner/mocha/spec-runner.js',
 
-    'examples-test-runner-mocha': './examples/test-runner/mocha.js',
-    'examples-editor': './examples/editor.js',
-    'examples-tddbin-standalone': './examples/tddbin-standalone.js',
+    'examples/test-runner/mocha': './examples/test-runner/mocha.js',
+    'examples/editor': './examples/editor.js',
+    'examples/tddbin-standalone': './examples/tddbin-standalone.js',
   },
   output: {
     path: path.join(__dirname, 'dist'),
