590483b461c68eb97d7d050f75b2dae91aa354c4
Georg Lang <georglang0788@gmx.net>
2014-09-04 10:36:29 +0200

    added orion set- and getContent

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -10,23 +10,15 @@ Editor.prototype = {
   _init: function () {
     document.getElementById(this._domNodeId).style.fontSize = '12px';
     document.getElementById(this._domNodeId).style.backgroundColor = 'white';
-    this._editor = editor;
-    console.log('_editor', this._editor);
-    this._editor.orion.editor.edit({parent: this._domNodeId});
-
+    this._editor = editor.orion.editor.edit({parent: this._domNodeId});
   },
 
-  //set and get replace with orion setter and getter
-
   setContent: function (content) {
-//      this._editor.selectAll();
-//      this._editor.insert(content);
-//    this._editor.TextView(options)
-//    this._editor.setText(content);
+    this._editor.setText(content);
   },
 
   getContent: function () {
-//      return this._editor.getValue()
+    return this._editor.getText();
   },
 
   insertAtCursorPosition: function (s) {
