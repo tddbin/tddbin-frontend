caf6908a40ca2cbfb93b7375a77fdc2f3656b8fb
Wolfram Kriesing <wk@uxebu.com>
2014-09-16 16:06:59 +0200

    A bit better comment to understand better what this page is for.

--- examples/editor.js
+++ examples/editor.js
@@ -2,7 +2,10 @@ var Editor = require('../src/editor/editor');
 var ShortcutManager = require('../src/keyboard-shortcut/shortcut-manager');
 
 var editor = new Editor('editorNode');
-editor.setContent('// comment');
+editor.setContent([
+  '// This just demonstrates the working editor component.',
+  '// If you press Meta+S `save it` should be console logged.'
+].join('\n'));
 
 var manager = new ShortcutManager();
 manager.registerShortcut(['Meta', 'S'], function() {
