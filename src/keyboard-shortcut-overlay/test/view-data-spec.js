6afa040fbcef7fc81d3c078fd2c532d32f1c7cf2
Wolfram Kriesing <wk@uxebu.com>
2014-09-24 11:37:45 +0200

    Simple implementation of detecting a key combo.

--- src/keyboard-shortcut-overlay/test/view-data-spec.js
+++ src/keyboard-shortcut-overlay/test/view-data-spec.js
@@ -11,16 +11,39 @@
 
  */
 
-function shallComponentBeVisible() {
+function shallComponentBeVisible(registeredShortcuts, pressedPartialShortcut) {
+  if (registeredShortcuts.length > 0) {
+    if (pressedPartialShortcut[0] == registeredShortcuts[0][0][0]) {
+      return true;
+    }
+  }
   return false;
+}
+
+function registerShortcuts(shortcuts) {
+  registeredShortcuts = shortcuts.map(function(shortcut) {
+    // Register the shortcut and all irrelevenat data.
+    return [shortcut, function() {}, shortcut.join(' + ')]
+  });
 }
 
 var registeredShortcuts = [];
 
 describe('NO key combo is pressed yet', function() {
-  it('should return visible=false', function() {
+  it('should tell component NOT to be visible', function() {
     var keyCombo = [];
     var visible = shallComponentBeVisible(registeredShortcuts, keyCombo);
     expect(visible).toBe(false);
+  });
+});
+
+describe('VALID first key of key combo is pressed', function() {
+
+  describe('should tell component to be visible', function() {
+    it('first registered shortcut matches', function() {
+      registerShortcuts([['Meta', 'S']]);
+      var isVisible = shallComponentBeVisible(registeredShortcuts, ['Meta']);
+      expect(isVisible).toBe(true);
+    });
   });
 });
