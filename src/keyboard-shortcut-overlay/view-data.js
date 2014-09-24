a31eba9f0fb792cdd799f28bb5147daa2a4d879a
Wolfram Kriesing <wk@uxebu.com>
2014-09-24 17:21:58 +0200

    Remove duplicated code.

--- src/keyboard-shortcut-overlay/view-data.js
+++ src/keyboard-shortcut-overlay/view-data.js
@@ -13,17 +13,8 @@ var viewData = {
     return shortcuts.filter(containsPressedKey);
   },
 
-  shallComponentBeVisible: function(registeredShortcuts, pressedPartialShortcut) {
-    if (pressedPartialShortcut.length == 0) {
-      return false;
-    }
-    function containsPressedKey(shortcutData) {
-      var shortcut = shortcutData[0];
-      return pressedPartialShortcut.every(function(key, idx) {
-        return key == shortcut[idx];
-      });
-    }
-    return registeredShortcuts.some(containsPressedKey);
+  shallComponentBeVisible: function(shortcuts, pressedKeys) {
+    return viewData.getMatchingShortcuts(shortcuts, pressedKeys).length > 0;
   }
 };
 
