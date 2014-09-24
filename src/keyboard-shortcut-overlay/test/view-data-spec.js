5575e642fddc913dc6d9be88c7d99e8576954a55
Wolfram Kriesing <wk@uxebu.com>
2014-09-24 09:17:44 +0200

    Write down all todos for the shortcut overlay data layer.

---
+++ src/keyboard-shortcut-overlay/test/view-data-spec.js
@@ -0,0 +1,18 @@
+/*
+- when NO key combo is pressed yet
+- when INVALID key combo is pressed
+  - visible shall be false
+
+- when a VALID key combo is pressed
+  - show all shortcuts filtered by the keycombo
+  - set visible=true
+
+  - pre-process shortcuts into ui-shortcuts (or alike) where 'Meta' is replace by '⌘' etc.
+
+ */
+
+describe('provide all data for the view', function() {
+  it('', function() {
+
+  });
+});
