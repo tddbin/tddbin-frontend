2776ea106787675714dad65d5bbad3b14ad94df7
Wolfram Kriesing <wk@uxebu.com>
2014-09-24 17:17:48 +0200

    Filtering is just a subset of the visible detection.

--- src/keyboard-shortcut-overlay/test/filter-shortcuts-spec.js
+++ src/keyboard-shortcut-overlay/test/filter-shortcuts-spec.js
@@ -5,10 +5,16 @@
  */
 
 function getMatchingShortcuts(shortcuts, pressedKeys) {
-  if (pressedKeys.length) {
-    return shortcuts[0][0];
+  if (pressedKeys.length == 0) {
+    return [];
   }
-  return [];
+  function containsPressedKey(shortcutData) {
+    var shortcut = shortcutData[0];
+    return pressedKeys.every(function(key, idx) {
+      return key == shortcut[idx];
+    });
+  }
+  return shortcuts.filter(containsPressedKey);
 }
 
 function registerShortcuts(shortcuts) {
@@ -30,7 +36,29 @@ describe('filter shortcuts by given key-combo', function() {
   it('should return just the one that matches', function() {
     var shortcut = ['Meta', 'S'];
     registerShortcuts([shortcut]);
-    expect(getMatchingShortcuts(registeredShortcuts, ['Meta'])).toEqual(shortcut);
+    expect(getMatchingShortcuts(registeredShortcuts, ['Meta'])).toEqual(registeredShortcuts);
+  });
+
+  it('should return all that match', function() {
+    var shortcuts = [['Meta', 'S'], ['Meta', 'F']];
+    registerShortcuts(shortcuts);
+    expect(getMatchingShortcuts(registeredShortcuts, ['Meta'])).toEqual(registeredShortcuts);
+  });
+
+  describe('should return only those that match', function() {
+    it('when one key pressed', function() {
+      var shortcuts = [['Meta', 'S'], ['Ctrl', 'S'], ['Meta', 'F']];
+      registerShortcuts(shortcuts);
+      var expected = [
+        registeredShortcuts[0], registeredShortcuts[2]
+      ];
+      expect(getMatchingShortcuts(registeredShortcuts, ['Meta'])).toEqual(expected);
+    });
+    it('when two keys pressed', function() {
+      var shortcuts = [['Meta', 'S'], ['Ctrl', 'S'], ['Meta', 'F']];
+      registerShortcuts(shortcuts);
+      expect(getMatchingShortcuts(registeredShortcuts, ['Ctrl', 'S'])).toEqual([registeredShortcuts[1]]);
+    });
   });
 
 
