1c7272ec87c649da423e41e50dae439c0625ff70
Wolfram Kriesing <wk@uxebu.com>
2014-10-13 17:41:45 +0200

    Simplify backend example.

--- examples/tddbin-with-backend.js
+++ examples/tddbin-with-backend.js
@@ -1,47 +1,28 @@
-var exampleTests = require('./example-tests');
-var EditorAndRunner = require('../src/editor-and-runner/controller');
-var NavigationBar = require('../src/navigation-bar/controller');
-var ShortcutManager = require('../src/keyboard-shortcut/shortcut-manager');
-var ShortcutOverlay = require('../src/keyboard-shortcut-overlay/overlay');
-var reqwest = require('reqwest');
-
 var $ = document.getElementById.bind(document);
-var editorAndRunner;
+var exampleTests = require('./example-tests');
+var Main = require('../src/main/main-controller');
+var util = require('./_util');
+var aceDefaultShortcuts = require('./_aceDefaultShortcuts');
 
-var executeTestCode = function() {
-  editorAndRunner.runEditorContent();
-  onStartUp()
-};
+var shortcuts = aceDefaultShortcuts.concat([
+  util.getShortcutObject([util.metaKey, 'S'], executeTestCode, 'Save+Run'),
+  util.getShortcutObject(['Shift', 'F6'], refactoringRename, 'Rename (refactoring)')
+]);
 
-new NavigationBar($('navigation-bar'), executeTestCode);
-
-editorAndRunner = new EditorAndRunner($('editor-and-runner'), '../src/test-runner/mocha/spec-runner.html');
-editorAndRunner.setEditorContent(exampleTests.simplePassingTestCode);
-
-
-// shortcut stuff
-
-var isMac = navigator.platform.indexOf('Mac') === 0;
-var providedByAceEditor = function() {/* noop() */};
-var metaKey = isMac ? 'Meta' : 'Control';
-var shortcuts = [
-  [[metaKey, 'S'], executeTestCode, 'Save+Run'],
-  [[metaKey, 'D'], providedByAceEditor(), 'Delete line'],
-  [[metaKey, 'Shift', 'D'], providedByAceEditor(), 'Duplicate line']
-];
-
-var manager = new ShortcutManager();
-manager.registerShortcuts(shortcuts);
-
-var shortcutsAndHints = shortcuts.map(function(shortcut) {
-  return {keys: shortcut[0], helpText: shortcut[2]};
+var main = new Main($('tddbin'), {
+  initialContent: exampleTests.simplePassingTestCode,
+  iframeSrcUrl: '../src/test-runner/mocha/spec-runner.html',
+  shortcuts: shortcuts
 });
-var overlay = new ShortcutOverlay($('keyboard-shortcut-overlay'));
-overlay.render(shortcutsAndHints);
 
-manager.onPossibleShortcut(overlay.show.bind(overlay));
-manager.onShortcutEnd(overlay.hide.bind(overlay));
+function executeTestCode() {
+  main.runEditorContent();
+}
+function refactoringRename() {
+  main.turnOnRenameMode();
+}
 
+var reqwest = require('reqwest');
 function onStartUp() {
   console.log('Fire ...');
   reqwest({
