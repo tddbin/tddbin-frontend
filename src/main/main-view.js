4f1648f491b4650ce0dd1ad3f94947df26dfd23a
Wolfram Kriesing <wk@uxebu.com>
2014-11-14 20:02:19 +0100

    Don't separate editor-and-runner out into it's own component, adding the session name at the top would not fit in the naming schema, and its to minimal right now.

--- src/main/main-view.js
+++ src/main/main-view.js
@@ -1,6 +1,5 @@
 /** @jsx React.DOM */
 var NavigationBar = require('../navigation-bar/navigation-bar-view');
-var EditorAndRunner = require('../editor-and-runner/editor-and-runner-view');
 var KeyboardShortcutOverlay = require('../keyboard-shortcut-overlay/keyboard-shortcut-overlay-view');
 
 var React = require('react');
@@ -15,10 +14,12 @@ var View = React.createClass({
           metaKeySymbol={props.metaKeySymbol}
           onSave={props.onSave}
         />
-        <EditorAndRunner
-          editorId={props.editorId}
-          runnerId={props.runnerId}
-        />
+
+        <div className="editor-and-runner">
+          <div id={this.props.editorId} className="editor"></div>
+          <div id={this.props.runnerId} className="runner"></div>
+        </div>
+
         <KeyboardShortcutOverlay
           metaKeySymbol={props.metaKeySymbol}
           shortcuts={props.shortcuts}
