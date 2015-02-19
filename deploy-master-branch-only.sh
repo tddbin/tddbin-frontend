eac88e2d41b00eed20af15e9976985e4d6a839a0
Wolfram Kriesing <wk@uxebu.com>
2015-02-19 03:05:47 +0100

    Fix bash command.

--- deploy-master-branch-only.sh
+++ deploy-master-branch-only.sh
@@ -2,7 +2,7 @@
 
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-if [ $CURRENT_BRANCH == "master" ]; then
+if [ "$CURRENT_BRANCH" == "master" ]; then
   npm run build
   ./deploy-to-ghpages.sh
 fi
