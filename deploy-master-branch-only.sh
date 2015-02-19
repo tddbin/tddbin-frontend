c55c1870235affd66422d8839a91e63e7dd0507f
Wolfram Kriesing <wk@uxebu.com>
2015-02-19 10:14:23 +0100

    Be verbose about what deployment does, so I can see where travis stops.

--- deploy-master-branch-only.sh
+++ deploy-master-branch-only.sh
@@ -2,7 +2,13 @@
 
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
+echo "deploy: current branch is '$CURRENT_BRANCH'"
+
 if [ "$CURRENT_BRANCH" == "master" ]; then
+  echo "deploy: let's run the build"
   npm run build
+  echo "deploy: run 'deploy-to-ghpages.sh'"
   ./deploy-to-ghpages.sh
+else
+  echo "deploy: nothing to deploy. (only deploying master branch)"
 fi
