d9cbb7ddf24a47428c8e16c40ae963e337395943
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-02-19 17:22:23 +0100

    Try to be more explicit and verbose.

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -10,12 +10,17 @@ TMP_CHECKOUT_DIR=tmp/origin-gh-pages
 (
   git clone --branch=gh-pages "https://${GH_TOKEN}@${GH_REF}" $TMP_CHECKOUT_DIR
 
+  echo "deploy: checked out 'gh-pages' branch"
   rm -rf $TMP_CHECKOUT_DIR/*
   cp -r dist/* $TMP_CHECKOUT_DIR
   cd $TMP_CHECKOUT_DIR
+  echo "deploy: configuring git user+email"
   git config user.name "Travis-CI"
   git config user.email "contact+travis-for-tddbin@uxebu.com"
+  echo "deploy: adding files"
   git add .
+  echo "deploy: committing"
   git commit -m "Travis deployed 'master' - `date`"
-  git push
+  echo "deploy: push"
+  git push "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages
 )
