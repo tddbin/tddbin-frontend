74ca99cfe60c4a6079ba97973b08735beb63035e
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-02-19 17:20:01 +0100

    Identify before doing changes in git.

--- deploy-to-ghpages.sh
+++ deploy-to-ghpages.sh
@@ -8,13 +8,13 @@ npm run build;
 
 TMP_CHECKOUT_DIR=tmp/origin-gh-pages
 (
-  git config user.name "Travis-CI"
-  git config user.email "contact+travis-for-tddbin@uxebu.com"
   git clone --branch=gh-pages "https://${GH_TOKEN}@${GH_REF}" $TMP_CHECKOUT_DIR
 
   rm -rf $TMP_CHECKOUT_DIR/*
   cp -r dist/* $TMP_CHECKOUT_DIR
   cd $TMP_CHECKOUT_DIR
+  git config user.name "Travis-CI"
+  git config user.email "contact+travis-for-tddbin@uxebu.com"
   git add .
   git commit -m "Travis deployed 'master' - `date`"
   git push
