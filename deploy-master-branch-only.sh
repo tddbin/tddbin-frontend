4f09510293e7a9026766f84938c50b6d0353bdbd
Wolfram Kriesing <wk@uxebu.com>
2015-02-19 10:58:15 +0100

    Document what we do here.

--- deploy-master-branch-only.sh
+++ deploy-master-branch-only.sh
@@ -3,6 +3,8 @@
 # travis does a `git checkout -qf <hash>` which detaches it from the branch
 # and `git branch` would show `* (detached from <hash>)` but we want the line below
 # of what `git branch` reports
+# TODO actually this is pretty ugly, since it is a hard-coded travis dependency, if
+# travis does it differently one day or we use another deploy service we may be screwed
 CURRENT_BRANCH=$(git branch | sed -n -e 's/^\  \(.*\)/\1/p')
 
 echo "deploy: current branch is '$CURRENT_BRANCH'"
