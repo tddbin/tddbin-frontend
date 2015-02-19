d0022c208c0731ba115f03d7acf2f64bf810d8ab
Wolfram Kriesing <wk@uxebu.com>
2015-02-19 10:33:59 +0100

    Try to be better determining the branch we are in, for some reason the last version returned "HEAD" on travis, but "master" when run locally.

--- deploy-master-branch-only.sh
+++ deploy-master-branch-only.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+CURRENT_BRANCH=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 echo "deploy: current branch is '$CURRENT_BRANCH'"
 
 if [ "$CURRENT_BRANCH" == "master" ]; then
-  echo "deploy: let's run the build"
+  echo "deploy: build via 'npm run build'"
   npm run build
   echo "deploy: run 'deploy-to-ghpages.sh'"
   ./deploy-to-ghpages.sh
