4383afc7734b785e381bc6c372db28ece4a2fdc1
Wolfram Kriesing <wk@uxebu.com>
2015-02-20 01:26:11 +0100

    Remove UI controls that are not needed now.

--- src/navigation-bar/navigation-bar-view.js
+++ src/navigation-bar/navigation-bar-view.js
@@ -8,12 +8,7 @@ var View = React.createClass({
     return (
       <header className="navigation-bar">
         <button className="logo"></button>
-        <button className="icon new" title="Start new TDD session" onClick={this.props.onNew}>New</button>
         <button className="icon save" title="Save and Run tests (⌘S)" onClick={this.props.onSave}>Save and Run ({this.props.metaKeySymbol}S)</button>
-        <div className="account">
-          {this.props.username}
-          <button className="button">Signout</button>
-        </div>
       </header>
     );
   }
