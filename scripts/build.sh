03719ac007c4bb3e65f877d78ec321037b29c4b7
Wolfram Kriesing <wk@uxebu.com>
2015-02-22 23:50:25 +0100

    Add favicon (also to build process). Move building the html stuff out of package.json.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-npm run build-html
+BUILD_DIR=dist
+
+rm -Rf $BUILD_DIR;
+mkdir $BUILD_DIR;
+cp html/index.html $BUILD_DIR;
+cp html/favicon.ico $BUILD_DIR;
+mkdir $BUILD_DIR/mocha/;
+cp src/test-runner/mocha/spec-runner.html $BUILD_DIR/mocha
+
 npm run build-app
 npm run build-css
 npm run build-ace
