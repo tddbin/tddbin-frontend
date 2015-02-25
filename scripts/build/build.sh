be9bf565026ed17244a4d20b36eabe0bb37a9ee6
Wolfram Kriesing <wk@uxebu.com>
2015-02-25 18:02:56 +0100

    Make build stop on any error in the chain. Cleaned it up a bit (i hope).

---
+++ scripts/build/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# TODO: must be run from project root (i am sure this is not bash best pratice)
+
+ORIGIN_ROOT="."
+DIST_ROOT="$ORIGIN_ROOT/dist"
+DIST_MOCHA_DIR="$DIST_ROOT/mocha"
+
+# clean up
+rm -Rf $DIST_ROOT;
+
+# create build directory (structure)
+mkdir -p $DIST_MOCHA_DIR;
+
+# copy html assets
+cp $ORIGIN_ROOT/html/index.html $DIST_ROOT;
+cp $ORIGIN_ROOT/html/favicon.ico $DIST_ROOT;
+cp $ORIGIN_ROOT/src/test-runner/mocha/spec-runner.html $DIST_MOCHA_DIR;
+
+# run all build scripts, `&&` ensures to stop on any fail
+(
+  npm run build-app &&
+  npm run build-css &&
+  npm run build-ace &&
+  npm run build-spec-runners
+)
+cp $ORIGIN_ROOT/CNAME $DIST_ROOT/CNAME;
