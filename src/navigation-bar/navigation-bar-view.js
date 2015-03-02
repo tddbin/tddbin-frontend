6c933a1536aa6c71dc1e716d74e72421e512ae70
Wolfram Kriesing <wk@uxebu.com>
2015-03-02 17:11:12 +0100

    Add links to the services used around tddbin.

--- src/navigation-bar/navigation-bar-view.js
+++ src/navigation-bar/navigation-bar-view.js
@@ -7,6 +7,11 @@ var View = React.createClass({
       <header className="navigation-bar">
         <button className="logo"></button>
         <button className="icon save" title="Run tests (⌘S)" onClick={this.props.onSave}>Run tests ({this.props.metaKeySymbol}S)</button>
+
+        <a href="http://uxebu.com" className="icon uxebu" title="Made by uxebu."></a>
+        <a href="http://twitter.com/tddbin" className="icon twitter" title="Get in touch."></a>
+        <a href="http://github.com/tddbin/tddbin-frontend" className="icon github" title="Get (into) the code and constribute."></a>
+        <a href="https://trello.com/b/FW1gUVxe/tddbin-com" className="icon trello" title="Vote, add features, discuss, ..."></a>
       </header>
     );
   }
