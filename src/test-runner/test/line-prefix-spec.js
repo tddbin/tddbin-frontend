047554f8dea101e33b8580f7b21c65927e7be5ca
Wolfram Kriesing <wk@uxebu.com>
2015-03-04 16:33:34 +0100

    Move out code+tests.

---
+++ src/test-runner/test/line-prefix-spec.js
@@ -0,0 +1,26 @@
+import {assert} from '../../_test-helper/assert';
+import {LinePrefix} from '../line-prefix.js';
+
+describe('get a line prefix for pretty printing', function() { // there must be `function` here so we don't inherit `this` from global :)
+
+  const getPrefix = LinePrefix.getPrefix;
+
+  describe('for max one digit line numbers', () => {
+    beforeEach(() => { this.maxDigits = 1; });
+    it('line number 1', () => assert.equal(getPrefix(1, this.maxDigits), '  1 | '));
+    it('line number 9', () => assert.equal(getPrefix(9, this.maxDigits), '  9 | '));
+  });
+  describe('for max two digit line numbers', () => {
+    beforeEach(() => { this.maxDigits = 2; });
+    it('line number 1', () => assert.equal(getPrefix(1, this.maxDigits), '   1 | '));
+    it('line number 19', () => assert.equal(getPrefix(19, this.maxDigits), '  19 | '));
+  });
+  describe('for max three digit line numbers', () => {
+    beforeEach(() => { this.maxDigits = 3; });
+    it('line number 1', () => assert.equal(getPrefix(1, this.maxDigits), '    1 | '));
+    it('line number 99', () => assert.equal(getPrefix(99, this.maxDigits), '   99 | '));
+    it('line number 100', () => assert.equal(getPrefix(100, this.maxDigits), '  100 | '));
+  });
+  it('maxDigits = 4, line number 1', () => assert.equal(getPrefix(1, 4), '     1 | '));
+  it('maxDigits = 4, line number 1234', () => assert.equal(getPrefix(1234, 4), '  1234 | '));
+});
---
+++ src/test-runner/test/marked-line-prefix-spec.js
@@ -0,0 +1,12 @@
+import {assert} from '../../_test-helper/assert';
+import {MarkedLinePrefix} from '../marked-line-prefix.js';
+
+describe('MarkedLinePrefix', () => {
+
+  const getPrefix = MarkedLinePrefix.getPrefix;
+
+  describe('has the `>` in front', () => {
+    it('line number 1', () => assert.equal(getPrefix(1, 1), '> 1 | '));
+    it('line number 1234', () => assert.equal(getPrefix(1234, 4), '> 1234 | '));
+  });
+});
