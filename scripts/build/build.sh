2609581e550dd1ac7bc5ec6d0bdd13765bc04ebb
Wolfram Kriesing <wk@uxebu.com>
2015-03-05 01:13:24 +0100

    Make build process support jasmine runner.

--- scripts/build/build.sh
+++ scripts/build/build.sh
@@ -5,17 +5,20 @@
 ORIGIN_ROOT="."
 DIST_ROOT="$ORIGIN_ROOT/dist"
 DIST_MOCHA_DIR="$DIST_ROOT/mocha"
+DIST_JASMINE_DIR="$DIST_ROOT/jasmine"
 
 # clean up
 rm -Rf $DIST_ROOT;
 
 # create build directory (structure)
 mkdir -p $DIST_MOCHA_DIR;
+mkdir -p $DIST_JASMINE_DIR;
 
 # copy html assets
 cp $ORIGIN_ROOT/html/index.html $DIST_ROOT;
 cp $ORIGIN_ROOT/html/favicon.ico $DIST_ROOT;
 cp $ORIGIN_ROOT/src/test-runner/mocha/spec-runner.html $DIST_MOCHA_DIR;
+cp $ORIGIN_ROOT/src/test-runner/jasmine/spec-runner.html $DIST_JASMINE_DIR;
 
 # run all build scripts, `&&` ensures to stop on any fail
 (
