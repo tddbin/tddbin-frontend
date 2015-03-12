92a0524dacbe7594b91a64adf2a7fc1dcbdad92c
Wolfram Kriesing <wk@uxebu.com>
2015-03-12 18:11:18 +0100

    Do offline by replacing URLs for requiring stuff.

--- scripts/build/build-offline-dev.sh
+++ scripts/build/build-offline-dev.sh
@@ -4,4 +4,15 @@ ORIGIN_ROOT="."
 DIST_ROOT="$ORIGIN_ROOT/dist"
 
 cp -r $ORIGIN_ROOT/vendor $DIST_ROOT;
-cp $ORIGIN_ROOT/html/index-offline.html $DIST_ROOT;
+
+# replace online refs using offline-refs (so I can work offline too)
+if [[ $OSTYPE == darwin* ]]; then
+  # replace in index.html
+  sed -i '' "s/\/\/cdn.rawgit.com\/jpillora/..\/vendor/g" $DIST_ROOT/index.html
+  # replace in spec-runners
+  sed -i '' "s/\/\/cdnjs.cloudflare.com\/ajax\/libs/..\/vendor/g" $DIST_ROOT/mocha/spec-runner.html
+  sed -i '' "s/\/\/cdnjs.cloudflare.com\/ajax\/libs/..\/vendor/g" $DIST_ROOT/jasmine/spec-runner.html
+else
+  echo "To do: make '$0' work on non-Macs";
+  exit -1;
+fi;
