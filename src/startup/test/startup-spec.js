be139785ee1b3d8fe4ca35f8f019acb350301213
Wolfram Kriesing <wk@uxebu.com>
2015-03-18 15:59:18 +0100

    Test getting the code from localstorage.

--- src/startup/test/startup-spec.js
+++ src/startup/test/startup-spec.js
@@ -11,13 +11,26 @@ import {startUp} from '../startup';
 
 const noop = function() {};
 
-describe('start up', () => {
-  it('remove kata from the hash', () => {
+describe('start up', function() {
 
+  it('remove kata from the hash', () => {
     global.window.location.hash = '#?kata=somekata';
 
     startUp(noop, (_, _1, onSuccess) => onSuccess(''));
 
     assert.equal(global.window.location.hash, '#?');
   });
+
+  it('if there is no kata in the URL get it from localStorage', function() {
+    global.window.location.hash = '#?';
+    global.localStorage = {
+      getItem: () => 'kata code'
+    };
+    const withSourceCode = this.sinon.spy();
+
+    startUp(withSourceCode, noop);
+
+    assert.calledWith(withSourceCode, 'kata code');
+  });
+
 });
