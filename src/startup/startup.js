a11a2543e24282cc8211d1f03042bf5ff64144f5
Wolfram Kriesing <wk@uxebu.com>
2015-04-16 18:01:59 +0200

    Everything into a class.

--- src/startup/startup.js
+++ src/startup/startup.js
@@ -1,47 +1,3 @@
-export const startUp = function(withKataSourceCode, xhrGet, xhrGetDefaultKata) {
-
-  const queryString = window.location.hash.replace(/^#\?/, '');
-
-  const getSourceCode = () => {
-    var kataUrl = getKataUrl();
-    var sourceCode = localStorage.getItem('code');
-    if (kataUrl) {
-      loadKataFromUrl(kataUrl, withKataSourceCode);
-    } else if (sourceCode) {
-      withKataSourceCode(sourceCode);
-    } else {
-      loadDefaultKata(withKataSourceCode);
-    }
-    window.location.hash = window.location.hash.replace(/kata=([^&]+)/, '');
-  };
-
-  const loadDefaultKata = (onLoaded) => {
-    xhrGetDefaultKata(
-      (_, {status}) => onLoaded(`// Kata at "${kataUrl}" not found (status ${status})\n// Maybe try a different kata (see URL).`),
-      data => {onLoaded(data)}
-    );
-  };
-
-  const loadKataFromUrl = (kataUrl, onLoaded) => {
-    xhrGet(
-      kataUrl,
-      (_, {status}) => onLoaded(`// Kata at "${kataUrl}" not found (status ${status})\n// Maybe try a different kata (see URL).`),
-      data => {onLoaded(data)}
-    );
-  };
-
-  const getKataUrl = () => {
-    var kataName = queryString.match(/kata=([^&]+)/);
-    if (kataName && kataName.length === 2) {
-      kataName = kataName[1];
-      return `http://${process.env.KATAS_SERVICE_DOMAIN}/katas/${kataName}.js`;
-    }
-  };
-
-  getSourceCode();
-
-};
-
 export default class StartUp {
   constructor(xhrGet, xhrGetDefaultKata) {
     this.xhrGet = xhrGet;
@@ -49,6 +5,45 @@ export default class StartUp {
   }
 
   loadSourceCode(withSourceCode) {
-    startUp(withSourceCode, this.xhrGet, this.xhrGetDefaultKata);
+    const {xhrGet, xhrGetDefaultKata} = this;
+    const queryString = window.location.hash.replace(/^#\?/, '');
+
+    const getSourceCode = () => {
+      var kataUrl = getKataUrl();
+      var sourceCode = localStorage.getItem('code');
+      if (kataUrl) {
+        loadKataFromUrl(kataUrl, withSourceCode);
+      } else if (sourceCode) {
+        withSourceCode(sourceCode);
+      } else {
+        loadDefaultKata(withSourceCode);
+      }
+      window.location.hash = window.location.hash.replace(/kata=([^&]+)/, '');
+    };
+
+    const loadDefaultKata = (onLoaded) => {
+      xhrGetDefaultKata(
+        (_, {status}) => onLoaded(`// Kata at "${kataUrl}" not found (status ${status})\n// Maybe try a different kata (see URL).`),
+        data => {onLoaded(data)}
+      );
+    };
+
+    const loadKataFromUrl = (kataUrl, onLoaded) => {
+      xhrGet(
+        kataUrl,
+        (_, {status}) => onLoaded(`// Kata at "${kataUrl}" not found (status ${status})\n// Maybe try a different kata (see URL).`),
+        data => {onLoaded(data)}
+      );
+    };
+
+    const getKataUrl = () => {
+      var kataName = queryString.match(/kata=([^&]+)/);
+      if (kataName && kataName.length === 2) {
+        kataName = kataName[1];
+        return `http://${process.env.KATAS_SERVICE_DOMAIN}/katas/${kataName}.js`;
+      }
+    };
+
+    getSourceCode();
   }
 }
