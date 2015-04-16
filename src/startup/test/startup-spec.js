dd98d14ac689a3bb0438c41260d4749646fc77d5
Wolfram Kriesing <wk@uxebu.com>
2015-04-16 18:30:29 +0200

    Removing almost all external deps to location.hash.

--- src/startup/test/startup-spec.js
+++ src/startup/test/startup-spec.js
@@ -20,7 +20,7 @@ describe('start up', function() {
   it('remove kata from the hash', () => {
     global.window.location.hash = '#?kata=somekata';
 
-    loadSourceCode('', noop, (_, _1, onSuccess) => onSuccess(''));
+    loadSourceCode('somekata', noop, (_, _1, onSuccess) => onSuccess(''));
 
     assert.equal(global.window.location.hash, '#?');
   });
@@ -34,17 +34,15 @@ describe('start up', function() {
     });
 
     it('if kata param is given but empty', function() {
-      global.window.location.hash = '#?kata=';
-
-      loadSourceCode('', withSourceCode, noop);
+      const kataUrl = '';
+      loadSourceCode(kataUrl, withSourceCode, noop);
 
       assert.calledWith(withSourceCode, 'kata code');
     });
 
     it('if there is no kata in the URL', function() {
-      global.window.location.hash = '#?';
-
-      loadSourceCode('', withSourceCode, noop);
+      const kataUrl = '';
+      loadSourceCode(kataUrl, withSourceCode, noop);
 
       assert.calledWith(withSourceCode, 'kata code');
     });
@@ -53,7 +51,6 @@ describe('start up', function() {
   describe('get default kata', function() {
 
     it('if there is no kata in the URL and nothing in localStorage', function() {
-      global.window.location.hash = '#?';
       global.localStorage = {getItem: noop};
       const xhrGetDefaultKata = this.sinon.spy();
 
@@ -67,7 +64,6 @@ describe('start up', function() {
 
   describe('request fails', function() {
     it('loads source code with error message', function() {
-      global.window.location.hash = '#?kata=some';
       const withSourceCode = this.sinon.spy();
       const status = 404;
       var kataUrl = 'some';
