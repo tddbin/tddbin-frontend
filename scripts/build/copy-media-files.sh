51d3bf161892342fa0f3ffe2a7f9854006d7525f
Wolfram Kriesing <wk@uxebu.com>
2015-05-04 21:35:08 +0200

    Moved around all media files, css+images.

--- scripts/build/copy-media-files.sh
+++ scripts/build/copy-media-files.sh
@@ -3,19 +3,10 @@
 # TODO: must be run from project root (i am sure this is not bash best pratice)
 
 ORIGIN_ROOT=".";
-MEDIA_FILES_ORIGIN_DIR="$ORIGIN_ROOT/src";
+MEDIA_FILES_ORIGIN_DIR="$ORIGIN_ROOT/src/components";
 DIST_ROOT="$ORIGIN_ROOT/dist";
 MEDIA_DIST_ROOT="$DIST_ROOT/media";
 
-# find all media files, under `src`
-cd "$MEDIA_FILES_ORIGIN_DIR";
-MEDIA_FILES=`find . \( -name '*.css' -o -name '*.png' -o -name '*.svg' -o -name '*.gif' \)`
-cd ..;
-
-# create all dirs where media files will land inside `dist/media`
-# copy the media file into the destination folder
-for MEDIA_FILE in $MEDIA_FILES
-do
-  mkdir -p `dirname "$MEDIA_DIST_ROOT/$MEDIA_FILE"`
-  cp "$MEDIA_FILES_ORIGIN_DIR/$MEDIA_FILE" "$MEDIA_DIST_ROOT/$MEDIA_FILE"
-done
+mkdir -p "$MEDIA_DIST_ROOT"
+cp $MEDIA_FILES_ORIGIN_DIR/*.css $MEDIA_DIST_ROOT
+cp -r $MEDIA_FILES_ORIGIN_DIR/img $MEDIA_DIST_ROOT
