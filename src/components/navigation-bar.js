dfe9815735c8fffe43a6414132d7222b18017b82
Wolfram Kriesing <wk@uxebu.com>
2015-05-04 18:11:57 +0200

    Uxebu logo is the left most logo. Show the keyboard shortcut on hover.

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -7,13 +7,13 @@ export default class NavigationBar extends React.Component {
     return (
       <header className="navigation-bar">
         <button className="logo"></button>
-        <button className="icon save" title="Run tests (⌘S)" onClick={onSave}>Run tests ({metaKeySymbol}S)</button>
+        <button className="save" title={`Run tests (${metaKeySymbol}S)`} onClick={onSave}>Run tests</button>
         <button title="Reset code" onClick={onResetCode}>Reset code</button>
 
-        <a href="http://uxebu.com" className="icon uxebu" title="Made by uxebu."></a>
         <a href="http://twitter.com/tddbin" className="icon twitter" title="Get in touch."></a>
         <a href="http://github.com/tddbin/tddbin-frontend" className="icon github" title="Get (into) the code and contribute."></a>
         <a href="https://trello.com/b/FW1gUVxe/tddbin-com" className="icon trello" title="Vote, add features, discuss, ..."></a>
+        <a href="http://uxebu.com" className="icon uxebu" title="Made by uxebu."></a>
       </header>
     );
   }
