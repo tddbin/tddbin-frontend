992a9fd70c61f58a13e9ae9f8db297a07ae11fd9
Wolfram Kriesing <wk@uxebu.com>
2015-05-24 01:15:53 +0200

    Make the list of all katas a scrollable div.

--- src/components/katas-navigation.js
+++ src/components/katas-navigation.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 export default class KatasNavigation extends React.Component {
 
+  onMouseOver(evt) {
+    console.log(evt);
+  }
+
   render() {
     return (
       <div id="katas-navigation">
-        <a href="/#?kata=some/kata" title="">#1</a>
-        <a href="/#?kata=some/kata" title="">#2</a>
+        <span>ES6 Katas</span>
+        <div className="scrollable" onMouseOver={this.onMouseOver.bind(this)}>
+          {[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20].map(id => <a href="/#?kata=some/kata" title={`Kata ${id}`}>#{id}</a>)}
+        </div>
       </div>
     );
   }
