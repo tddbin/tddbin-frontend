f850f514fd5dbb4985d359efc24b8798b1039e56
Wolfram Kriesing <wk@uxebu.com>
2015-05-24 00:25:40 +0200

    Styling the katas navigation a bit.

--- src/components/katas-navigation.js
+++ src/components/katas-navigation.js
@@ -5,7 +5,8 @@ export default class KatasNavigation extends React.Component {
   render() {
     return (
       <div id="katas-navigation">
-        jo katas ... man
+        <a href="/#?kata=some/kata" title="">#1</a>
+        <a href="/#?kata=some/kata" title="">#2</a>
       </div>
     );
   }
