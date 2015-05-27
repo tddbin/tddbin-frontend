17eb884af2c889e1e633f8946c6917f6f07e3a7e
Wolfram Kriesing <wk@uxebu.com>
2015-05-28 00:50:59 +0300

    KataUrl needs to tell if the URL is a es6kata.

--- src/_test/kata-url-spec.js
+++ src/_test/kata-url-spec.js
@@ -12,5 +12,22 @@ describe('get kata from katas.tddbin.com', function() {
     assert.equal(KataUrl.fromQueryString(kataUrlParam), expectedUrl);
   });
 
+  describe('if no valid kata is given', function() {
+    it('returns a new instance', function() {
+      assert.ok(KataUrl.fromQueryString('') instanceof KataUrl);
+    });
+    it('toString() returns en empty string', function() {
+      const kataUrl = KataUrl.fromQueryString('').toString();
+      assert.equal(kataUrl, '');
+    });
+  });
+
 });
 
+describe('report if a kata URL is a ES6 kata', function() {
+  it('true if kata starts with `es6/language/`', function() {
+    const kataUrlParam = 'kata=es6/language/...';
+    const kataUrl = KataUrl.fromQueryString(kataUrlParam);
+    assert.equal(kataUrl.isEs6Kata, true);
+  });
+});
--- src/kata-url.js
+++ src/kata-url.js
@@ -1,10 +1,29 @@
 /* global process */
 export default class KataUrl {
+
+  constructor() {
+    this.kataName = '';
+  }
+
   static fromQueryString(queryString) {
     var kataName = queryString.match(/kata=([^&]+)/);
     if (kataName && kataName.length === 2) {
-      kataName = kataName[1];
-      return `http://${process.env.KATAS_SERVICE_DOMAIN}/katas/${kataName}.js`;
+
+      let kataUrl = new KataUrl();
+      kataUrl.kataName = kataName[1];
+      return kataUrl;
     }
+    return new KataUrl();
+  }
+
+  toString() {
+    if (this.kataName) {
+      return `http://${process.env.KATAS_SERVICE_DOMAIN}/katas/${this.kataName}.js`;
+    }
+    return '';
+  }
+
+  get isEs6Kata() {
+    return this.kataName.startsWith('es6/language/');
   }
 }
