974e339dfeae4253fcf9538c75e687c4227a9f32
Wolfram Kriesing <wk@uxebu.com>
2015-06-19 19:14:01 +0200

    Hack out a version which works with a real url query string.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,34 @@ import {shortcuts as aceDefaultShortcuts} from './_aceDefaultShortcuts';
 import StartUp from './startup';
 import {xhrGet} from './_external-deps/xhr.js';
 import KataUrl from './kata-url.js';
+import querystring from 'querystring';
 
-const onSave = () => main.onSave();
+// get all globals
+
+function setInUrlQuery(key, value) {
+  const query = querystring.parse(window.location.search.replace(/^\?/, ''));
+  query[key] = value;
+  window.history.pushState(null, {}, `?${querystring.stringify(query)}`);
+}
+
+function onSave() {
+  setInUrlQuery('storedLocally', 1);
+  window.localStorage.setItem('code', main._editor.getContent());
+  main.runEditorContent();
+}
+
+function onReset() {
+  window.localStorage.removeItem('code');
+  window.location.reload();
+}
 
 const shortcuts = aceDefaultShortcuts.concat([
   getShortcutObject([metaKey, 'S'], onSave, 'Save+Run')
 ]);
 
 const appDomNode = document.getElementById('tddbin');
-var main = new MainController(appDomNode, {
+var main = new MainController(appDomNode, onSave, onReset);
+main.configure({
   iframeSrcUrl: `./mocha/spec-runner.html`,
   shortcuts: shortcuts
 });
@@ -30,10 +49,14 @@ var xhrGetDefaultKata = xhrGet.bind(null, DEFAULT_KATA_URL);
 
 const startUp = new StartUp(xhrGet, xhrGetDefaultKata);
 
-const queryString = window.location.hash.replace(/^#\?/, '');
-var kataUrl = KataUrl.fromQueryString(queryString);
+const queryStringInHash = window.location.hash.replace(/^#\?/, '');
+if (queryStringInHash) {
+  window.history.pushState(null, {}, `?${queryStringInHash}`);
+}
+const query = querystring.parse(window.location.search.replace(/^\?/, ''));
+var kataUrl = KataUrl.fromKataName(query.kata);
 
-startUp.loadSourceCode(kataUrl.toString(), withSourceCode);
+startUp.loadSourceCode(query.storedLocally, kataUrl.toString(), withSourceCode);
 function onSuccess(es6KataData) {
   main.showEs6KatasNavigation(es6KataData);
 }
---
+++ src/url/test/url-spec.js
@@ -0,0 +1,11 @@
+//import assert from '../../_test-helper/assert.js';
+//
+//describe('url', function() {
+//  describe('with a kata hash', function() {
+//    it('redirects to a URL with a query', function() {
+//      //let hash = '#?kata=hello/world';
+//
+//
+//    });
+//  });
+//});
