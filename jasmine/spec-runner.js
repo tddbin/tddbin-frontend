6cbcb2a36019772c3a1d1e75205eb88581314464
Travis-CI <contact+travis-for-tddbin@uxebu.com>
2015-07-02 08:34:02 +0000

    Travis deployed 'master' - Thu Jul  2 08:34:02 UTC 2015

--- jasmine/spec-runner.js
+++ jasmine/spec-runner.js
@@ -1,5 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+'use strict';
 
 var jasmine = window.jasmine;
 var env = jasmine.getEnv();
@@ -11,6 +11,6 @@ function consumeMessage(messageData) {
   env.execute();
 }
 
-window.addEventListener("message", consumeMessage, false);
+window.addEventListener('message', consumeMessage, false);
 
 },{}]},{},[1]);
