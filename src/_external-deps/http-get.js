e6e35fd20f018abc4bda356280d38b27c0391f37
Wolfram Kriesing <wk@uxebu.com>
2015-07-19 22:54:45 +0200

    Reducing complexity a bit.

--- src/_external-deps/http-get.js
+++ src/_external-deps/http-get.js
@@ -3,26 +3,39 @@ import https from 'https';
 import url from 'url';
 
 export function loadRemoteFile(fileUrl, onLoaded) {
+  const module = httpOrHttpsModule(fileUrl);
+  const options = buildOptions(fileUrl);
+  const request = module.request(options, processResponse.bind(null, onLoaded));
+  request.on('error', onLoaded);
+  request.end();
+}
+
+function httpOrHttpsModule(fileUrl) {
   const isHttps = fileUrl.toLowerCase().startsWith('https');
-  const httpModule = (isHttps ? https : http);
-  var data = '';
+  return (isHttps ? https : http);
+}
+
+function processResponse(onLoaded, res) {
+  const statusCode = res.statusCode;
+  if (statusCode === 200) {
+    handle200Response(res, onLoaded);
+    return;
+  }
+  const message = `HTTP request failed with status code ${statusCode}`;
+  onLoaded(new Error(message));
+}
+
+function handle200Response(res, onLoaded) {
+  let data = '';
+  res.on('data', (chunk) => { data += chunk; });
+  res.on('end', () => { onLoaded(null, data); });
+}
+
+function buildOptions(fileUrl) {
   var options = url.parse(fileUrl);
   options.headers = {
     'User-Agent': '' // api.github.com wants a user-agent header
   };
   options.withCredentials = false;
-  var request = httpModule.request(options, function(res) {
-    if (res.statusCode !== 200) {
-      onLoaded(new Error(`HTTP request failed with status code ${res.statusCode}`));
-    } else {
-      res.on('data', function(chunk) { data += chunk; });
-      res.on('end', function() {
-        onLoaded(null, data);
-      });
-    }
-  });
-  request.on('error', function(e) {
-    onLoaded(e);
-  });
-  request.end();
+  return options;
 }
