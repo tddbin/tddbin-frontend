7bf9ddab80e489410fa0b85d7cd576e738724959
Wolfram Kriesing <wk@uxebu.com>
2015-07-19 21:52:40 +0200

    Some more basic intergrated tests.

--- src/_external-deps-tests/http-get-spec.js
+++ src/_external-deps-tests/http-get-spec.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import {loadRemoteFile} from '../_external-deps/http-get.js';
 
-describe('loadRemoteFile', function() {
+describe('loadRemoteFile, integrated test, that REQUIRE to be online', function() {
 
   describe('loading a kata from katas.tddbin.com', function() {
     it('works', function(done) {
@@ -23,11 +23,22 @@ describe('loadRemoteFile', function() {
   });
 
   describe('loading code from gist.github.com', function() {
-    it('works', function() {
-
+    it('works', function(done) {
+      const url = 'https://api.github.com/gists/a046034f74679e2d4057';
+      loadRemoteFile(url, (err, data) => {
+        assert.equal(err, null);
+        const fileContent = JSON.parse(data).files['test.js'].content;
+        assert.equal(fileContent.startsWith('// just a test'), true);
+        done();
+      });
     });
-    it('returns an error for wrong URL', function() {
-
+    it('returns an error for wrong URL', function(done) {
+      const url = 'https://api.github.com/gists/invalid-gist';
+      loadRemoteFile(url, (err, data) => {
+        assert.equal(data, void 0);
+        assert.equal(('' + err).includes('404'), true);
+        done();
+      });
     });
   });
 });
