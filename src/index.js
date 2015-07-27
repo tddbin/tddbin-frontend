b00ce1f259d4d466805cd390dbcd2fbf0748b214
Wolfram Kriesing <wk@uxebu.com>
2015-07-27 17:06:37 +0200

    Loading a site using ?kataName=... or ?gistId=... works.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,44 @@
 /* global process */
+
+// load content ONLY
+import 'babel/polyfill';
+import MainController from './main-controller';
+import {getShortcutObject, metaKey} from './_util';
+import {shortcuts as aceDefaultShortcuts} from './_aceDefaultShortcuts';
+
+const onSave = () => main.onSave();
+
+const shortcuts = aceDefaultShortcuts.concat([
+  getShortcutObject([metaKey, 'S'], onSave, 'Save+Run')
+]);
+
+const appDomNode = document.getElementById('tddbin');
+var main = new MainController(appDomNode, {
+  iframeSrcUrl: `./mocha/spec-runner.html`,
+  shortcuts: shortcuts
+});
+main.render();
+
+const withSourceCode = (sourceCode) => {
+  main.setEditorContent(sourceCode);
+  setTimeout(onSave, 1000);
+};
+
+
+import SourceCodeContent from './load-code-at-startup.js';
+import {loadRemoteFile} from './_external-deps/http-get.js';
+import {parse as parseQuerystring} from 'querystring';
+
+const query = parseQuerystring(window.location.search.replace(/^\?/, ''));
+
+var noop = () => {};
+new SourceCodeContent(loadRemoteFile, noop)
+  .load(query, withSourceCode, (err) => {
+    console.log(err);
+  });
+
+
+/*
 import MainController from './main-controller';
 import {getShortcutObject, metaKey} from './_util';
 import {shortcuts as aceDefaultShortcuts} from './_aceDefaultShortcuts';
@@ -41,3 +81,4 @@ function onSuccess(es6KataData) {
 if (kataUrl.isEs6Kata) {
   xhrGet(`http://${process.env.KATAS_SERVICE_DOMAIN}/katas/es6/language/__all__.json`, function() {}, onSuccess);
 }
+*/
