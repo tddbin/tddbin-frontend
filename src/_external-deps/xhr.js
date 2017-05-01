6ee4f3f8f08922a98f27e3bb72f0a60de19ece07
Wolfram Kriesing <wolfram+github@kriesing.de>
2017-05-01 23:35:55 +0200

    Seems some browsers want this ...

--- src/_external-deps/xhr.js
+++ src/_external-deps/xhr.js
@@ -1,7 +1,7 @@
 import atomic from 'atomic';
 
 export function xhrGet(url, onError, onSuccess) {
-  atomic.ajax({method: 'GET', url: url, responseType: 'document'})
+  atomic.ajax({method: 'GET', url: url, responseType: ''})
     .success(onSuccess)
     .error(onError)
   ;
