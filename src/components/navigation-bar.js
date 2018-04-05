c51494842085694826a7247adf3753b866ed5cfc
wolframkriesing <wolfram+github@kriesing.de>
2018-04-05 14:27:27 +0200

    Switch (partly) to felxbox.

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -12,14 +12,20 @@ export default class NavigationBar extends React.Component {
     } = this.props;
     return (
       <header className="navigation-bar">
-        <button className="logo" />
-        <span className="tddbin">TDD bin</span>
-        <button className="save" title={`Run tests (${metaKeySymbol}S)`} onClick={onSave}>Run tests</button>
-        <button title="Reset code" onClick={onResetCode}>Reset code</button>
-        Transpile to ES5 <input type="checkbox" onChange={(evt) => onTranspileToEs5Changed(evt.target.checked)} />
+        <span style={{flex: 1}}>
+          <button className="logo" />
+          <span className="tddbin">TDD bin</span>
+          <button className="save" title={`Run tests (${metaKeySymbol}S)`} onClick={onSave}>Run tests</button>
+          <button title="Reset code" onClick={onResetCode}>Reset code</button>
+          <span className="transpileSwitch">
+            Transpile to ES5 <input type="checkbox" onChange={(evt) => onTranspileToEs5Changed(evt.target.checked)} />
+          </span>
+        </span>
 
-        <a href={TWITTER_URL} className="icon twitter" title="Get in touch." />
-        <a href={GITHUB_URL} className="icon github" title="Get (into) the code and contribute." />
+        <span>
+          <a href={TWITTER_URL} className="icon twitter" title="Get in touch." />
+          <a href={GITHUB_URL} className="icon github" title="Get (into) the code and contribute." />
+        </span>
       </header>
     );
   }
