968a4ec31958e1767ff37a6a314bab4139eeb8c7
wolframkriesing <wolfram+github@kriesing.de>
2018-04-05 18:00:45 +0200

    Draw a tiny arrow, to see better that the hint is a hint :).

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -36,17 +36,20 @@ export default class NavigationBar extends React.Component {
           <label className="transpileSwitch">
             Transpile to ES5 {transpileCheckbox}
             <div className="transpileHint">
-              <p className={this.state.transpileToEs5 ? 'highlight' : ''}>
-                <strong>on</strong> - transpiles ES6 (and beyond) into ES5<br />
-                Why turn it off? To see if the browser does implement this specific feature
-                of the JavaScript language.
-              </p>
-              <p className={this.state.transpileToEs5 ? '' : 'highlight'}>
-                <strong>off</strong> - leaves code as is and runs tests (does NOT transpile code to ES5)<br />
-                Why turn it on? Things like <code>import assert from 'assert'</code> would not work
-                if they don't get transpiled to ES5 code, since the <code>import</code> statement is only allowed on
-                  the top level of a JS file.
-              </p>
+              <div className="arrow"> </div>
+              <div className="content">
+                <p className={this.state.transpileToEs5 ? 'highlight' : ''}>
+                  <strong>on</strong> - transpiles ES6 (and beyond) into ES5<br />
+                  Why turn it off? To see if the browser does implement this specific feature
+                  of the JavaScript language.
+                </p>
+                <p className={this.state.transpileToEs5 ? '' : 'highlight'}>
+                  <strong>off</strong> - leaves code as is and runs tests (does NOT transpile code to ES5)<br />
+                  Why turn it on? Things like <code>import assert from 'assert'</code> would not work
+                  if they don't get transpiled to ES5 code, since the <code>import</code> statement is only allowed on
+                    the top level of a JS file.
+                </p>
+              </div>
             </div>
           </label>
         </span>
