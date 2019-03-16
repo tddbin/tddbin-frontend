f0dcad6b41743bde07b4f9340250995f6f536205
wolframkriesing <wolfram+github@kriesing.de>
2019-03-16 21:46:33 +0100

    Test the transpile function.

--- src/test-runner/mocha/spec-runner-spec.js
+++ src/test-runner/mocha/spec-runner-spec.js
@@ -2,9 +2,22 @@ import assert from '../../_test-helper/assert';
 import {es6ToEs5Code} from './spec-runner';
 
 describe('Spec runner', () => {
-  it('transpiles code (the `import` statement)', () => {
-    const es6Code = 'import assert from "assert"';
-    const es5Code = es6ToEs5Code(es6Code);
-    assert.equal(es5Code.includes(es6Code), false, `Expected NOT to contain \`${es6Code}\``);
+  describe('WHEN transpileToEs5=true', () => {
+    const transpileToEs5 = (sourceCode) =>
+      es6ToEs5Code({sourceCode, transpileToEs5: true});
+    it('transpiles code (the `import` statement)', () => {
+      const es6Code = 'import assert from "assert"';
+      const es5Code = transpileToEs5(es6Code);
+      assert(es5Code.includes('require("assert")'), `Expected transpiled code to contain "require(assert)", was NOT found.`);
+    });
+  });
+  describe('WHEN transpileToEs5=false', () => {
+    const noopTranspile = (sourceCode) =>
+      es6ToEs5Code({sourceCode, transpileToEs5: false});
+    it('transpiles code (the `import` statement)', () => {
+      const es6Code = 'import assert from "assert";';
+      const es5Code = noopTranspile(es6Code);
+      assert.equal(es5Code, es6Code);
+    });
   });
 });
