17ea190268fb6f1e6b90567a0daed540c7daa22f
wolframkriesing <wolfram+github@kriesing.de>
2019-12-10 23:18:16 +0100

    Add docker for local dev.

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM node
+WORKDIR /app
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOCKERFILE_HASH=$(md5 -q ./Dockerfile)
+CONTAINER_NAME=tddbin
+IMAGE_NAME=${CONTAINER_NAME}-image:${DOCKERFILE_HASH}
+PORT=9778
+
+if [[ $(docker inspect --format . ${IMAGE_NAME} 2>&1) != "." ]]; then
+  echo "--- BUILDING image '${IMAGE_NAME}' ---"
+  docker build -t ${IMAGE_NAME} -f Dockerfile .
+fi
+
+echo "--- RUNNING container '${CONTAINER_NAME}' ---"
+docker run --rm \
+	--name ${CONTAINER_NAME} \
+	--publish 8080:${PORT} \
+	--volume $(pwd):/app \
+	--env "KATAS_SERVICE_URL=http://katas.tddbin.local/" \
+	--env "TDDBIN_FRONTEND_PORT=${PORT}" \
+	${IMAGE_NAME} $1
