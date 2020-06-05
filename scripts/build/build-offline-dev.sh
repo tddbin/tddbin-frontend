713c1148079782d5087bbfff6577ab10e394e704
shine <4771718+shinenelson@users.noreply.github.com>
2020-06-05 14:38:16 +0530

    unify javascript content delivery networks
    currently there are 4 CDNs that are being called for
    different javascript libraries. this means that there are
    4 different third-party requests being made on each load.
    this changeset unifies the multiple CDNs into a single
    provider - jsDelivr.net
    
    why do it?
    the advantage of unifying all of the libraries from a single
    provider is that all of the requests can be served off a
    single connection if the connection is made over HTTP/2
    essentially making the load times at least 2 times faster.
    
    why jsDelivr?
    jsDelivr has a wide network spanning [multiple caching
    network providers](https://www.jsdelivr.com/network) whereas
    the other providers rely on a single caching network ( unpkg,
    cdnjs - cloudflare, github - fastly ). while technically all
    the requests could probably be served from the same backend
    cache, since the requests are being proxied via different
    domains, naturally, they have to go through different
    connections. if all the requests are proxied through the same
    domain and via HTTP/2, the requests can all be served via the
    same connection making the page load faster.
    
    other changes
    - synced the library version for `ace/ext-language_tools`
    with the loaded `ace.js` library
    
    room for improvement
    - `react` and `ace-builds` libraries could be upgraded to the
    latest versions. since I wasn't sure whether this would cause
    any breakage in the application, I didn't attempt an upgrade.
    I just used the already existing version but just changed the
    provider URL. Maybe a minor version upgrade ( react ) would
    not do much harm? ( I didn't want to take the risk since it
    was react )

--- scripts/build/build-offline-dev.sh
+++ scripts/build/build-offline-dev.sh
@@ -8,9 +8,7 @@ cp -r $ORIGIN_ROOT/vendor $DIST_ROOT;
 # replace online refs using offline-refs (so I can work offline too)
 if [[ $OSTYPE == darwin* ]]; then
   # replace in index.html
-  sed -i'' "s/\/\/cdn.rawgit.com\/jpillora/..\/vendor/g" $DIST_ROOT/index.html
   sed -i'' "s/\/\/cdn.jsdelivr.net/..\/vendor/g" $DIST_ROOT/index.html
-  sed -i'' "s/\/\/cdnjs.cloudflare.com\/ajax\/libs/..\/vendor/g" $DIST_ROOT/index.html
   # replace in spec-runners
   sed -i'' "s/\/\/cdnjs.cloudflare.com\/ajax\/libs/..\/vendor/g" $DIST_ROOT/mocha/spec-runner.html
   sed -i'' "s/\/\/cdnjs.cloudflare.com\/ajax\/libs/..\/vendor/g" $DIST_ROOT/jasmine/spec-runner.html
